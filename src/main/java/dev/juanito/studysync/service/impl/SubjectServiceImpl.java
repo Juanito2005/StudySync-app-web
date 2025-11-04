7b68179906c49de1032f2e6ef46a2b4a03b4e8d3
Juan Camilo Garcia Cuevas <juanca.cuevas.05@outlook.com>
2025-11-04 17:47:44 +0100

    The CRUD for Subject has been created, also the JwtServiceTest has been created

--- src/main/java/dev/juanito/studysync/service/impl/SubjectServiceImpl.java
+++ src/main/java/dev/juanito/studysync/service/impl/SubjectServiceImpl.java
@@ -1,89 +1,90 @@
-// package dev.juanito.studysync.service.impl;
+package dev.juanito.studysync.service.impl;
 
-// import org.springframework.security.core.Authentication;
-// import org.springframework.security.core.context.SecurityContextHolder;
-// import org.springframework.stereotype.Service;
+import java.util.Optional;
 
-// import dev.juanito.studysync.dto.SubjectCreationDto;
-// import dev.juanito.studysync.dto.SubjectUpdateDto;
-// import dev.juanito.studysync.exception.SubjectIdNotFoundException;
-// import dev.juanito.studysync.exception.SubjectNameAlreadyExistsException;
-// import dev.juanito.studysync.exception.UserIdNotFoundException;
-// import dev.juanito.studysync.model.Subject;
-// import dev.juanito.studysync.model.User;
-// import dev.juanito.studysync.repository.SubjectRepository;
-// import dev.juanito.studysync.repository.UserRepository;
-// import dev.juanito.studysync.security.UserPrincipal;
-// import dev.juanito.studysync.service.SubjectService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
-// @Service
-// public class SubjectServiceImpl implements SubjectService{
+import dev.juanito.studysync.dto.SubjectCreationDto;
+import dev.juanito.studysync.dto.SubjectUpdateDto;
+import dev.juanito.studysync.exception.SubjectIdNotFoundException;
+import dev.juanito.studysync.exception.SubjectNameAlreadyExistsException;
+import dev.juanito.studysync.exception.UserIdNotFoundException;
+import dev.juanito.studysync.model.Subject;
+import dev.juanito.studysync.model.User;
+import dev.juanito.studysync.repository.SubjectRepository;
+import dev.juanito.studysync.repository.UserRepository;
+import dev.juanito.studysync.security.UserPrincipal;
+import dev.juanito.studysync.service.SubjectService;
 
-//     private final SubjectRepository subjectRepository;
-//     private final UserRepository userRepository;
+@Service
+public class SubjectServiceImpl implements SubjectService{
 
-//     public SubjectServiceImpl(SubjectRepository subjectRepository, UserRepository userRepository) {
-//         this.subjectRepository = subjectRepository;
-//         this.userRepository = userRepository;
-//     }
+    private final SubjectRepository subjectRepository;
+    private final UserRepository userRepository;
 
-//     private Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//     private UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
-//     User user = userRepository.findById(userPrincipal.getUserId())
-//         .orElseThrow(() -> new UserIdNotFoundException("The user Id has not been found"));
+    public SubjectServiceImpl(SubjectRepository subjectRepository, UserRepository userRepository) {
+        this.subjectRepository = subjectRepository;
+        this.userRepository = userRepository;
+    }
 
-//     @Override
-//     public Subject createSubject(SubjectCreationDto subjectCreationDto) {
+    // A Security Context is created for avoid the mess of being calling the db all the time
+    private User getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        return userRepository.findById(userPrincipal.getUserId())
+            .orElseThrow(() -> new UserIdNotFoundException("Authenticated user not found"));
+    }
 
-//         // Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//         // UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
-//         // User user = userRepository.findById(userPrincipal.getUserId())
-//         //     .orElseThrow(() -> new UserIdNotFoundException("The user Id has not been found"));
-//         checkIfSubjectNameExists(subjectCreationDto.getName());
-//         Subject subject = new Subject();
-//         subject.setName(subjectCreationDto.getName());
-//         subject.setColorHEX(subjectCreationDto.getColor());
-//         subject.setUser(user);
-//         return subjectRepository.save(subject);
-//     }
+    @Override
+    public Subject createSubject(SubjectCreationDto subjectCreationDto) {
+        User user = getAuthenticatedUser();
+        if (subjectRepository.findByNameAndUser(subjectCreationDto.getName(), user).isPresent()) {
+            throw new SubjectNameAlreadyExistsException("You already have a subject with this name");
+        }
+        Subject subject = new Subject();
+        subject.setName(subjectCreationDto.getName());
+        subject.setColorHEX(subjectCreationDto.getColor());
+        subject.setUser(user);
+        return subjectRepository.save(subject);
+    }
 
-//     public void checkIfSubjectNameExists(String name) {
-//         if (subjectRepository.findByName(name).isPresent()) {
-//             throw new SubjectNameAlreadyExistsException("Cannot be created a subject with an existing subject name");
-//         }
-//     }
+    @Override
+    public Subject findSubjectById(Long id) {
+        User user = getAuthenticatedUser();
+        return subjectRepository.findByIdAndUser(id, user)
+            .orElseThrow(() -> new SubjectIdNotFoundException("Subjectcnot found or access denied"));
+    }
 
-//     @Override
-//     public Subject findSubjectById(Long id) {
-//         // Is this ok?
-//         Subject subject = subjectRepository.findById(id)
-//             .orElseThrow(() -> new SubjectIdNotFoundException("The subject has not been found"));
-//         subject.setUser(user);
-//         return subject;
-//     }
+    @Override
+    public void deleteSubjectById(Long id) {
+        User user = getAuthenticatedUser();
+        // Since an Optional type is returned, the exceptions must been handle
+        Subject subject = subjectRepository.findByIdAndUser(id, user)
+            .orElseThrow(() -> new SubjectIdNotFoundException("Subject not found or access denied"));
+        subjectRepository.delete(subject);
+    }
 
-//     // How would the user know the subject's id? Does I need to create a Subject Dto for this?
-//     @Override
-//     public void deleteSubjectById(Long id) {
+    @Override
+    public Subject updateSubjectById(SubjectUpdateDto subjectUpdateDto, Long id) {
+        User user = getAuthenticatedUser();
 
-//         Subject subject = findSubjectById(id);
-//         subject.setUser(user);
-//         subjectRepository.delete(subject);
-//     }
+        Subject subject = subjectRepository.findByIdAndUser(id, user)
+            .orElseThrow(() -> new SubjectIdNotFoundException("Subject not found or access denied"));
 
-//     // How does the program knows that the subject's id given corresponds with the user's id?
-//     // Isn't this suppose to throw an Exception in the case any of the fields is null?
-//     @Override
-//     public Subject updateSubjectById(SubjectUpdateDto subjectUpdateDto, Long id) {
-//         Subject subject = findSubjectById(id);
-//         if (subjectUpdateDto.getName() != null) {
-//             checkIfSubjectNameExists(subjectUpdateDto.getName());
-//             subject.setName(subjectUpdateDto.getName());
-//         }
-//         if (subjectUpdateDto.getColor() != null) {
-//             subject.setColorHEX(subjectUpdateDto.getColor());
-//         }
-//         subject.setUser(user);
-//         return subjectRepository.save(subject);
-//     }
-// }
+        if (subjectUpdateDto.getName() != null) {
+            Optional<Subject> existingSubject = subjectRepository.findByNameAndUser(subjectUpdateDto.getName(), user);
+            if (existingSubject.isPresent() && !existingSubject.get().getId().equals(id)) {
+                throw new SubjectNameAlreadyExistsException("You already have a subject with this name");
+            }
+            subject.setName(subjectUpdateDto.getName());
+        }
+
+        if (subjectUpdateDto.getColor() != null) {
+            subject.setColorHEX(subjectUpdateDto.getColor());
+        }
+
+        return subjectRepository.save(subject);
+    }
+}
